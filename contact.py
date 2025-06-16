04beca13f0cf9f96e12573d3af55b7efe2fd7d39
Anna Knyazeva <anna.pyatkova.1996@gmail.com>
2025-06-16 19:37:31 +0700

    Добавлен вспомогательный класс

---
+++ contact.py
@@ -0,0 +1,28 @@
+
+class Contact:
+
+    def __init__(self, firstname, middlename, lastname, nickname, photo, title, company, address, home_phone,
+                       mobile_phone, work_phone, fax, email_1, email_2, email_3, homepage, bday, byear, aday, ayear,
+                       bmonth, amonth):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.photo = photo
+        self.title = title
+        self.company = company
+        self.address = address
+        self.home_phone = home_phone
+        self.mobile_phone = mobile_phone
+        self.work_phone = work_phone
+        self.fax = fax
+        self.email_1 = email_1
+        self.email_2 = email_2
+        self.email_3 = email_3
+        self.homepage = homepage
+        self.bday = bday
+        self.byear = byear
+        self.aday = aday
+        self.ayear = ayear
+        self.bmonth = bmonth
+        self.amonth = amonth
