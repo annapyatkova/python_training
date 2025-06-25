a764cec8c30da8af1850397e927ab22bd91b9d2a
Anna Knyazeva <anna.pyatkova.1996@gmail.com>
2025-06-25 15:25:30 +0700

    Выделен класс для управления сессиями

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -12,8 +12,8 @@ def app(request):
 
 
 def test_add_contact(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_contact(Contact(firstname="Anna1", middlename="Anna2", lastname="Anna3", nickname="Knyazeva", photo="C:\\Users\Ann\Downloads\cat.jpg", title="Training",
                         company="Software-Testing", address="Barnaul", home_phone="123456", mobile_phone="9111111111", work_phone="123", fax="qwerty", email_1="test1@test.ru",
                         email_2="test2@test.ru", email_3="test3@test.ru", homepage="test.ru", bday="1", byear="1911", aday="2", ayear="1922", bmonth="March", amonth="January"))
-    app.logout()
+    app.session.logout()
